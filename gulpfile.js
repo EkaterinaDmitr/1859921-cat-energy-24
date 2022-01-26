7b77b2d5ec3458c078520944765d640935078daa
EkaterinaDmitr <ketrins-lite_94@mail.ru>
2022-01-26 18:17:36 +0300

    исправила спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -71,12 +71,12 @@ const createWebp = () => {
 // SVG
 
 const svg = () =>
-  gulp.src(['source/img/*.svg', '!source/img/icons/*.svg'])
+  gulp.src(['source/img/*.svg', '!source/img/footer-svg/*.svg'])
     .pipe(svgo())
     .pipe(gulp.dest('build/img'));
 
 const sprite = () => {
-  return gulp.src('source/img/icons/*.svg')
+  return gulp.src('source/img/footer-svg/*.svg')
     .pipe(svgo())
     .pipe(svgstore({
       inlineSvg: true
